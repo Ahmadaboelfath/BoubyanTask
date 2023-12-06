371e94521d8df3abcecaa7e0c2e8a94a7b2ec2c3
ahmed aboel fath <ahmed.mahmoud6@vodafone.com>
2023-12-06 22:17:30 +0200

    first commit

---
+++ src/webparts/createRequest/CreateRequestWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+
+import * as strings from 'CreateRequestWebPartStrings';
+import CreateRequest from './components/CreateRequest';
+import { ICreateRequestProps } from './components/ICreateRequestProps';
+
+export interface ICreateRequestWebPartProps {
+  description: string;
+}
+
+export default class CreateRequestWebPart extends BaseClientSideWebPart<ICreateRequestWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<ICreateRequestProps > = React.createElement(
+      CreateRequest,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/createRequest/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ICreateRequestWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'CreateRequestWebPartStrings' {
+  const strings: ICreateRequestWebPartStrings;
+  export = strings;
+}
